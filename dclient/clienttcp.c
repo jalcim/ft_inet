d9617b005892947f0e891de4ff65d9d49589e451
jalcim <jalcim@student.42.fr>
2014-03-12 13:18:28 +0100

    test

--- dclient/clienttcp.c
+++ dclient/clienttcp.c
@@ -6,14 +6,14 @@
 /*   By: jalcim <jalcim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/13 00:03:39 by jalcim            #+#    #+#             */
-/*   Updated: 2014/03/12 10:41:54 by jalcim           ###   ########.fr       */
+/*   Updated: 2014/03/12 11:53:34 by jalcim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libsock/ft_inet.h"
 #include "../libft/libft.h"
 #define PORT 34000
-#define IP "127.0.0.1"
+#define IP "10.11.8.19"//"127.0.0.1"
 #define SIZE_FILENAME 256
 
 int ft_cli_socktcp(t_sockaddr_in *sin);
